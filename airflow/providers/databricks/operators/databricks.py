d7bcbf5cd1857ce6c9a23ebe088cd452f27741ef
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2023-10-11 14:34:38 +0100

    Fix DatabricksCreateJobsOperator support with dict payload
    tests/always/test_example_dags.py::test_should_be_importable[tests/system/providers/databricks/example_databricks.py]

--- airflow/providers/databricks/operators/databricks.py
+++ airflow/providers/databricks/operators/databricks.py
@@ -163,10 +163,18 @@ class DatabricksJobRunLink(BaseOperatorLink):
         return XCom.get_value(key=XCOM_RUN_PAGE_URL_KEY, ti_key=ti_key)
 
 
+def normalize_list(items_list: list) -> list:
+    """Normalizes list so all items are of dictionaries."""
+    if all(isinstance(item, dict) for item in items_list):
+        return items_list
+    return [item.as_dict() for item in items_list]
+
+
 class DatabricksCreateJobsOperator(BaseOperator):
     """
-    Creates (or resets) a Databricks job using the
-    `api/2.1/jobs/create
+    Creates (or resets) a Databricks job.
+
+    Uses `api/2.1/jobs/create
     <https://docs.databricks.com/dev-tools/api/latest/jobs.html#operation/JobsCreate>`_
     (or `api/2.1/jobs/reset
     <https://docs.databricks.com/dev-tools/api/latest/jobs.html#operation/JobsReset>`_)
@@ -256,9 +264,9 @@ class DatabricksCreateJobsOperator(BaseOperator):
         if tags is not None:
             self.json["tags"] = tags
         if tasks is not None:
-            self.json["tasks"] = [task.as_dict() for task in tasks]
+            self.json["tasks"] = normalize_list(tasks)
         if job_clusters is not None:
-            self.json["job_clusters"] = [job_cluster.as_dict() for job_cluster in job_clusters]
+            self.json["job_clusters"] = normalize_list(job_clusters)
         if email_notifications is not None:
             self.json["email_notifications"] = email_notifications.as_dict()
         if webhook_notifications is not None:
@@ -272,7 +280,7 @@ class DatabricksCreateJobsOperator(BaseOperator):
         if git_source is not None:
             self.json["git_source"] = git_source.as_dict()
         if access_control_list is not None:
-            self.json["access_control_list"] = [acl.as_dict() for acl in access_control_list]
+            self.json["access_control_list"] = normalize_list(access_control_list)
 
         self.json = normalise_json_content(self.json)
 
